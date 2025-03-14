61b61733e121ff0fed291316edcfa0f99a2fb507
GGalas10 <grzegorzgalas10@gmail.com>
2025-03-14 21:34:52 +0100

    In TaskService add checkSprintExist

--- ScrumMaster.Tasks.Infrastructure/Implementations/TaskService.cs
+++ ScrumMaster.Tasks.Infrastructure/Implementations/TaskService.cs
@@ -5,20 +5,25 @@ using ScrumMaster.Tasks.Infrastructure.Contracts;
 using ScrumMaster.Tasks.Infrastructure.DataAccess;
 using ScrumMaster.Tasks.Infrastructure.DTOs;
 using ScrumMaster.Tasks.Infrastructure.Exceptions;
+using System.Text.Json;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace ScrumMaster.Tasks.Infrastructure.Implementations
 {
     public class TaskService : ITaskService
     {
         private readonly ITaskDbContext _context;
-        public TaskService(ITaskDbContext context)
+        private readonly HttpClient _httpClient;
+        public TaskService(ITaskDbContext context, HttpClient httpClient)
         {
             _context = context;
+            _httpClient = httpClient;
         }
         public async Task<Guid> CreateTask(CreateTaskCommand command)
         {
             if (command == null)
                 throw new BadRequestException("Command_Cannot_Be_Null");
+            await CheckSprintExist(command.sprintId);
             var newTask = new TaskModel(command.title, command.description, command.sprintId);
             _context.Tasks.Add(newTask);
             await _context.SaveChangesAsync();
@@ -29,6 +34,8 @@ namespace ScrumMaster.Tasks.Infrastructure.Implementations
             if (command == null)
                 throw new BadRequestException("Command_Cannot_Be_Null");
             var oldTask = await _context.Tasks.FirstOrDefaultAsync(x => x.Id == command.oldTaskId);
+            if(command.sprintId != Guid.Empty)
+                await CheckSprintExist(command.sprintId);
             if (oldTask == null)
                 throw new BadRequestException("Cannot_Find_Task_To_Update");
             var anyChanges = oldTask.UpdateTask(command.title, command.description, command.status, command.sprintId, command.assignedUserId);
@@ -57,6 +64,16 @@ namespace ScrumMaster.Tasks.Infrastructure.Implementations
             var tasks = await _context.Tasks.Where(x => x.SprintId == sprintId).ToListAsync();
 
             return tasks.Select(x => TaskDTO.GetFromModel(x)).ToList();
+        }
+        private async Task CheckSprintExist(Guid sprintId)
+        {
+            var result = await _httpClient.GetAsync($"/Sprint/CheckSprintExist?sprintId={sprintId}");
+            if (result.IsSuccessStatusCode)
+            {
+                var sprintExist = JsonSerializer.Deserialize<bool>(await result.Content.ReadAsStringAsync());
+                if (!sprintExist)
+                    throw new Exception("Sprint_Cannot_Exist");
+            }
         }
     }
 }
