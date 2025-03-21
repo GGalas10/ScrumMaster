fd6598df14ef75d3ed1a80ceb00927b3c7cd8943
GGalas10 <grzegorzgalas10@gmail.com>
2025-03-21 17:51:47 +0100

    Add enviroment Many ideas for little time

--- scrum-master-ui/src/app/Core/Services/Auth.service.ts
+++ scrum-master-ui/src/app/Core/Services/Auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient, HttpStatusCode } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
+  apiUrl = environment.identityUrl;
+  constructor(private http: HttpClient) { }
+  RegisterUser(command: RegisterCommand): Observable<HttpStatusCode> {
+    return this.http.post(`${this.apiUrl}/Register`, command);
+  }
 }
---
+++ scrum-master-ui/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    identityUrl: 'https://localhost:2101',
+    sprintUrl: 'https://localhost:2102',
+    taskUrl: 'https://localhost:2103'
+  };
+  
