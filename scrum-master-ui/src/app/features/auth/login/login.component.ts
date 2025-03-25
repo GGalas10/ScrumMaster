ea781e3318912d22b925ec9fa3d2b1b0e83c57e2
GGalas10 <grzegorzgalas10@gmail.com>
2025-03-25 23:32:08 +0100

    Visual changes

--- scrum-master-ui/src/app/features/auth/login/login.component.ts
+++ scrum-master-ui/src/app/features/auth/login/login.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../../../Core/Services/Auth.service';
 import { LoginCommand } from '../../../Core/Models/UsersInterfaces';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   standalone: true,
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class LoginComponent {
   constructor(private authService:AuthService){}
+  private formBuilder = inject(FormBuilder);
+  form = this.formBuilder.group({
+    email: ['',Validators.compose([Validators.email, Validators.required])],
+    password: ['',Validators.compose([Validators.required,Validators.minLength(6)])]
+  });
   onSubmit(){
-    let command:LoginCommand = {email:"Test",password:"Test"};
+    if(!this.form.valid){
+      alert("Uzupełnij wszystkie pola");
+      return;
+    }
+    let command:LoginCommand = {email:this.form.value.email || "",password: this.form.value.password || ""};
+    console.log(command);
+    return;
     this.authService.LoginUser(command).subscribe({
       next: result => console.log(result),
       error: err => {
