27184f196a8384eda5eb61b5973fcbafad3bd59d
GGalas10 <grzegorzgalas10@gmail.com>
2025-03-29 23:04:02 +0100

    Adding custom alert logic Adding some errors text

--- scrum-master-ui/src/app/features/auth/register/register.component.ts
+++ scrum-master-ui/src/app/features/auth/register/register.component.ts
@@ -2,17 +2,22 @@ import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../Core/Services/Auth.service';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
+import { CustomAlertComponent } from "../../../shared/components/custom-alert/custom-alert.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule,TranslatePipe],
+  imports: [ReactiveFormsModule, TranslatePipe, CustomAlertComponent,CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
   standalone: true
 })
 export class RegisterComponent {
-  constructor(private authService:AuthService){}
   private formBuilder = inject(FormBuilder);
+  errors:string[]=[];
+  title:string = "";
+  showModal = false;
+  constructor(private authService:AuthService){}
   registerForm = this.formBuilder.group({
     firstName : ['',Validators.required],
     lastName : ['',Validators.required],
@@ -28,6 +33,20 @@ export class RegisterComponent {
       let errors:string[] = [];
       if(this.registerForm.controls['firstName'].invalid)
         errors.push("Errors.FirstNameRequired")
+      if(this.registerForm.controls['lastName'].invalid)
+        errors.push("Errors.LastNameRequired")
+      if(this.registerForm.controls['email'].invalid)
+        errors.push("Errors.EmaiRequired")
+      if(this.registerForm.controls['password'].invalid)
+        errors.push("Errors.PasswordRequired")
+      if(this.registerForm.controls['confirmPassword'].invalid)
+        errors.push("Errors.ConfirmPasswordRequired")
+      this.ShowErrorModal(errors,"Errors.FormInvalid")
     }
+  }
+  ShowErrorModal(errors:string[],title:string){
+    this.errors = errors;
+    this.title = title;
+    this.showModal = true;
   }
 }
--- scrum-master-ui/src/app/shared/components/custom-alert/custom-alert.component.ts
+++ scrum-master-ui/src/app/shared/components/custom-alert/custom-alert.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-custom-alert',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './custom-alert.component.html',
-  styleUrl: './custom-alert.component.scss'
+  styleUrl: './custom-alert.component.scss',
+  standalone : true
 })
 export class CustomAlertComponent {
  @Input() errors!:string[];
  @Input() title!:string;
+ @Output() closeEvent = new EventEmitter<void>();
+
+ CloseModal(){
+  this.closeEvent.emit();
+ }
 }
